0baef1299f67ae186bf496bca9f3b88d73d060ba
Enzo <enzowederma@gmail.com>
2022-06-28 17:31:40 -0300

    javascript: add difficulty parameter

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const result = document.getElementById('result');
 const input = document.getElementById('input');
 let score = 0;
 let streak = 0;
+let multiplier;
 let expireTime = 5000;
+let difficulty;
 
 // Random Key Chooser
 
@@ -33,31 +35,40 @@ function SetKey() {
 
 // Key Listeners & Checkers
 
+function MissKey () {
+    SetKey();
+    streak = 0;
+    UpdateScoreboard();
+    result.innerHTML = 'Missed one'
+    // Sets a new key to be pressed, nulify your streak, updates the scoreboard and warns you missed a key
+};
+
 function RightKey() {
     result.innerHTML = 'Nice one!';
-    IncreaseScore(streak);
+    IncreaseScore(multiplier);
     streak += 1;
-    // If you get a key right, you win 1 points times your streak and your streak is increased by one
+    // If you get a key right, you win 1 points times your streak, your streak is increased by one and the difficulty is increased
 };
 
 function WrongKey() {
     result.innerHTML = 'Not that one';
     streak = 0;
-    // If you miss a key, you lose your streak, but not points
+    difficulty -= 1;
+    // If you miss a key, the difficulty is decreased, you lose your streak, but not points
 };
 
 function CheckKeyPressed(event) {
     if (event.keyCode == key.code) {
         RightKey();
-        expireTime -= 5;
     }
     else {
         WrongKey();
-        expireTime = 5000;
     }
+
     SetKey();
     UpdateScoreboard();
-
+    setDifficulty();
+    setDifficultySettings();
     // Checks if the key you pressed is equal to the one solicited, reacting accordingly to the result, defines a new key to be pressed and updates your scoreboard
 };
 
@@ -77,16 +88,8 @@ function IncreaseScore(points) {
     // Increases your score in one point, if the points to be increased is zero is increases one instead
 };
 
-function MissKey () {
-    SetKey();
-    streak = 0;
-    UpdateScoreboard();
-    result.innerHTML = 'Missed one'
-    // Sets a new key to be pressed, nulify your streak, updates the scoreboard and warns you missed a key
-};
-
 function UpdateScoreboard() {
-    scoreboard.innerHTML = `Score: ${score} | Streak: ${streak} | Time Between Keys: ${expireTime}ms`;
+    scoreboard.innerHTML = `Score: ${score} | Streak: ${streak} | Time Between Keys: ${expireTime}ms | Difficulty: ${difficulty}` ;
     // Change the scoreboard HTML so it shows the current score and streak
 };
 
@@ -110,6 +113,64 @@ function ClearAllTimeouts() {
     }
 
     // Clears all timeouts that might be active at the time
+};
+
+
+
+// Difficulty Effects & Causes
+
+function setDifficulty() {
+    switch (true) {
+        case (streak < 10):
+            difficulty = 1;
+            break;
+        
+        case (streak < 350):
+            difficulty = 2;
+            break;
+        
+        case (streak < 1000):
+            difficulty = 3;
+            break;
+    
+        case (streak > 1000):
+            difficulty = 4;
+            break;
+    
+        default:
+        console.log('Something went wrong while setting difficulty via score');
+    };
+};
+
+function setDifficultySettings() {
+    switch (difficulty) {
+        case 1: 
+            expireTime = 5000;
+            multiplier = 1;
+            break;
+    
+        case 2:
+            expireTime = 3500;
+            multiplier = 2;
+            break;
+        
+        case 3:
+            expireTime = 2000;
+            multiplier = 4;
+            break;
+    
+        case 4:
+            expireTime = 1000;
+            multiplier = 5;
+            break;
+    
+        case (difficulty < 0):
+            difficulty = 0;
+            break;
+        
+        default:
+            console.log('Something went wrong while setting difficulty');
+    };
 };
 
 
