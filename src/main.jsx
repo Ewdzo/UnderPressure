31b8c378125071cb4d6c90227b7dd51b8d86aa0d
Enzo Weder <enzowederma@gmail.com>
2022-07-13 17:52:36 -0300

    react: add key prompts database

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import App from './App'
 import { Scoreboard }  from './components/scoreboard'
 import './index.css'
 import {score, streak, expireTime, difficulty, lifes, multiplier, highscore } from './Test'
+import { prompts } from './components/prompts'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <br />
     <Scoreboard score={score} streak={streak} time={expireTime} difficulty= {difficulty} lifes= {lifes} multiplier= {multiplier} highscore= {highscore} />
+    <div>{prompts[0].message}</div>
   </React.StrictMode>
 )
-  
