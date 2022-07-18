d210674aebbd2216c346e8a8c2972917cc56e3c6
Enzo Weder <enzowederma@gmail.com>
2022-07-18 18:01:47 -0300

    react: realocate prompt-related functionalities from App.jsx to prompts/index.jsx + add timeout for future lose functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,20 @@
 import './App.css'
 import Scoreboard from './components/scoreboard'
 import Player from './components/player'
-import Prompt from './components/prompts'
-import { useEffect, useState } from 'react'
+import PromptDiv from './components/prompts'
+import { useState } from 'react'
 
 
 function App() {
-  const [prompt, setPrompt] = useState('');
-
-  useEffect(() => {
-
-    function checkKey(event) {
-      if (event.key== prompt.code) {
-        console.log("Nice one!");
-      }
-      else {
-        console.log(event.key, prompt.code);
-      }
-    };
-
-    window.addEventListener("keydown", checkKey);
-    
-    return () => {
-      window.removeEventListener("keydown", checkKey);
-    };
-  }, [prompt]);
+  const [playing, setPlaying] = useState(false);
 
   return (
     <>
       {Player.name}
       <Scoreboard score={Player.score} streak= {Player.streak} difficulty= {Player.difficulty} lifes= {Player.lifes} multiplier= {Player.multiplier} highscore= {Player.highscore} />
-      <div id='prompt'>
-        <p>This: {prompt.message}</p>
-        <button onClick={() => setPrompt(Prompt())}>Start</button>
-      </div>
+      {playing
+        ? <PromptDiv />
+        : <div id='prompt'><button onClick={() => {setPlaying(true)}}>Test</button></div> }
     </>
   )
 };
--- src/components/prompts/index.jsx
+++ src/components/prompts/index.jsx
@@ -1,4 +1,5 @@
 import './prompts.css'
+import { useState, useEffect } from 'react'
 
 class Prompts {
     constructor(key) {
@@ -8,11 +9,11 @@ class Prompts {
         this.value = 1,
         this.difficulty = "easy",
         this.time = 5000,
-        this.code = key.toLowerCase()
+        this.code = key.charCodeAt(0)
     }
 }
 
-function Prompt() {
+function choosePrompt() {
     const randomNumber = (Math.floor(Math.random()*(25)) + 65)
     const randomLetter = String.fromCharCode(randomNumber)
     const randomPrompt = new Prompts(randomLetter)
@@ -22,4 +23,49 @@ function Prompt() {
     ) 
 };
 
-export default Prompt
+function PromptDiv() {
+   
+const [prompt, setPrompt] = useState('');
+
+function newPrompt() {
+    setPrompt(choosePrompt());
+
+    var id = window.setTimeout(function() {}, 0);
+    while (id--) {
+        window.clearTimeout(id);
+    }
+
+    const timer = setTimeout(() => console.log(prompt.time), prompt.time);
+  };
+
+  useEffect(() => {
+
+    function checkKey(event) {
+      if (event.keyCode == prompt.code) {
+        newPrompt();
+      }
+      else {
+        console.log(event.key, prompt.message);
+      }
+    };
+
+    window.addEventListener("keydown", checkKey);
+    
+    return () => {
+      window.removeEventListener("keydown", checkKey);
+    };
+  }, [prompt]);
+
+  useEffect(() => {
+    newPrompt()
+  }, []);
+
+    return(
+        <div id='prompt'>
+        <p>This: {prompt.message}</p>
+        <button onClick={() => newPrompt()}>Start</button>
+      </div>
+    )
+}
+
+export default PromptDiv
