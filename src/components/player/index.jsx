a02e0f53ce48c721d29c40a9eea1f04b36888027
Enzo Weder <enzowederma@gmail.com>
2022-07-21 00:28:34 -0300

    react: implement prototypes of functions to change players stats

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,41 +1,63 @@
-class Players {
-    constructor(score, streak, lifes, multiplier, highscore) {
-        this.name = "Ewdzo";
-        this.difficulty = Number("4");
-        this.score = score;
-        this.streak = streak;
-        this.lifes = lifes;
-        this.multiplier = multiplier;
-        this.highscore = highscore;
+import { Component } from "react";
 
-        switch (this.difficulty) {
-            case 0:
-                this.difficulty = "Easy";
-                break;
-            
-            case 1:
-                this.difficulty = "Normal";
-                break;
-            
-            case 2:
-                this.difficulty = "Medium"
-                break;
-
-            case 3:
-                this.difficulty = "Hard"
-                break;
-            
-            case 4:
-                this.difficulty = "Insane"
-                break;
-
-            default:
-                this.difficulty = "Invalid Difficulty"
-                alert("Invalid Difficulty, please choose from 0-4")
-        } 
-    }
+class Player extends Component {
+    state = {
+        name: "Null",
+        difficulty: "Easy",
+        score: 0,
+        streak: 0,
+        lifes: 3,
+        multiplier: 1,
+        highscore: 0}
 };
 
-const Ewdzo = new Players(0, 0, 3, 1, 10000)
+const newPlayer = (new Player).state
+export default newPlayer
 
-export default Ewdzo
+
+// Functions
+
+const incrementScore = (player, value) => {
+   player.score = player.score + value
+};
+
+const decrementScore = (player, value) => {
+    player.score = player.score - value
+};
+
+const incrementStreak = player => {
+    player.streak = player.streak + 1
+};
+
+const resetStreak = player => {
+    player.streak = 0
+};
+
+const incrementLife = (player, value) => {
+    player.lifes = player.streak + value
+};
+
+const decrementLife = (player, value) => {
+    player.lifes = player.streak - value
+};
+
+const incrementMultiplier = (player, value) => {
+    player.multiplier = player.multiplier + value
+};
+
+const decrementMultiplier = (player, value) => {
+    player.multiplier = player.multiplier - value
+};
+
+const resetMultiplier = player => {
+    player.multiplier = 0
+};
+
+
+function setHighscore(player) {
+    if (player.score > player.highscore) {
+        player.highscore = player.score
+    }
+}
+
+export { incrementScore, decrementScore, incrementStreak, resetStreak, incrementLife, decrementLife, incrementMultiplier, decrementMultiplier, resetMultiplier, setHighscore };
