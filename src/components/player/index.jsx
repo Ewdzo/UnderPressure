613a551042da05fb047f9b20fc2c8227e05f51ba
Enzo Weder <enzowederma@gmail.com>
2022-07-21 19:30:51 -0300

    react: remake and realocate player-stats-related functions - first functional prototype

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,14 +1,18 @@
 import './scoreboard.css'
-import { useState } from "react";
+import '../prompts/prompts.css'
+import { useEffect, useState } from "react";
+import generatePrompt from '../prompts';
 
 function PlayerStats() {
 
+    const [name, setName] = useState('PlayerTest')
     const [score, setScore] = useState(0)
     const [difficulty, setDifficulty] = useState('Easy')
     const [streak, setStreak] = useState(0)
     const [lifes, setLifes] = useState(3)
     const [multiplier, setMultiplier] = useState(1)
     const [highscore, setHighscore] = useState(0)
+    const [prompt, setPrompt] = useState({time: 5000});
 
 
     const incrementScore = value => {
@@ -43,16 +47,14 @@ function PlayerStats() {
         setMultiplier(prevMultiplier => prevMultiplier - value)
     };
 
-    const resetMultiplier = value => {
+    const resetMultiplier = () => {
         setMultiplier(0)
     };
 
     const defineHighscore = () => {
         if (highscore < score) {
-            highscore = score}
+            setHighscore(score)}
     }
-
-    const [name, setName] = useState('Ewdzo')
 
     const player = {
         name: name,
@@ -64,17 +66,62 @@ function PlayerStats() {
         highscore: highscore
     }
 
+    function newPrompt() {
+    setPrompt(generatePrompt());
+    
+    var id = window.setTimeout(function() {}, 0);
+    while (id--) {
+        window.clearTimeout(id);
+    };
+    
+        const timer = setTimeout(function() {
+            resetStreak() 
+            newPrompt()
+        },  prompt.time);
+    };
+    
+    useEffect(() => {
+    
+        function checkKey(event) {
+        if (event.keyCode == prompt.code) {
+            incrementScore(prompt.value);
+            incrementStreak(1)
+            newPrompt()
+        }
+        else {
+            resetStreak()
+            newPrompt()
+            decrementLife(1)
+        }
+        };
+    
+        window.addEventListener("keydown", checkKey);
+        
+        return () => {
+        window.removeEventListener("keydown", checkKey);
+        };
+    }, [prompt]);
+
+    
+    
     return(
-        <div id='scoreboard' style={{display: 'block'}}>
-            <div id='scoremulti'>
-                <div id='score'>Score: {player.score}</div>
-                <div id='multiplier'>{player.multiplier}x</div>
+        <>
+            <div id='scoreboard' style={{display: 'block'}}>
+                <div id='scoremulti'>
+                    <div id='score'>Score: {player.score}</div>
+                    <div id='multiplier'>{player.multiplier}x</div>
+                </div>
+                <div id='streak'>Streak: {player.streak}</div>
+                <div id='difficulty'>Difficulty: {player.difficulty}</div>
+                <div id='lifes'>Lifes: {player.lifes}</div>
+                <div id='highscore'>Highscore: {player.highscore}</div> 
             </div>
-            <div id='streak'>Streak: {player.streak}</div>
-            <div id='difficulty'>Difficulty: {player.difficulty}</div>
-            <div id='lifes'>Lifes: {player.lifes}</div>
-            <div id='highscore'>Highscore: {player.highscore}</div> 
-        </div>
+
+            <div id='prompt'>
+            <p>{prompt.message}</p>
+            <button onClick={() => newPrompt()}>Start</button>
+            </div>
+        </>
     )
 }
 
