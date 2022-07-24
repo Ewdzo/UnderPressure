3e460cff8f37de42c984d5310bd656e7f6617bc4
Enzo Weder <enzowederma@gmail.com>
2022-07-24 02:40:09 -0300

    react: remove unnecessary functions & add lose-by-time and lose-by-missing keys feature

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,26 +1,27 @@
 import './scoreboard.css'
 import '../prompts/prompts.css'
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import generatePrompt from '../prompts';
 
 function PlayerStats() {
 
-    const [name, setName] = useState('PlayerTest')
-    const [score, setScore] = useState(0)
-    const [difficulty, setDifficulty] = useState('Easy')
-    const [streak, setStreak] = useState(0)
-    const [lifes, setLifes] = useState(3)
-    const [multiplier, setMultiplier] = useState(1)
-    const [highscore, setHighscore] = useState(0)
+    const [name, setName] = useState('PlayerTest');
+    const [score, setScore] = useState(0);
+    const [difficulty, setDifficulty] = useState(0);
+    const [streak, setStreak] = useState(0);
+    const [lifes, setLifes] = useState(3);
+    const [multiplier, setMultiplier] = useState(1);
+    const [highscore, setHighscore] = useState(0);
     const [prompt, setPrompt] = useState({time: 5000});
-
+    const lifesRef = useRef(lifes);
+    lifesRef.current = lifes;
 
     const incrementScore = value => {
     setScore(prevScore => prevScore + value)
     };
 
-    const decrementScore = value => {
-        setScore(prevScore => prevScore - value)
+    const resetScore = value => {
+        setScore(0)
     };
 
     const incrementStreak = () => {
@@ -31,30 +32,30 @@ function PlayerStats() {
         setStreak(0)
     };
 
+    const resetLife = () => {
+        setLifes(3)
+    };
+
     const incrementLife = value => {
         setLifes(prevLifes => prevLifes + value)
     };
 
     const decrementLife = value => {
-        setLifes(prevLifes => prevLifes - value)
+     setLifes(prevLifes => prevLifes - value)
     };
 
     const incrementMultiplier = value => {
         setMultiplier(prevMultiplier => prevMultiplier + value)
     };
 
-    const decrementMultiplier = value => {
-        setMultiplier(prevMultiplier => prevMultiplier - value)
-    };
-
     const resetMultiplier = () => {
-        setMultiplier(0)
+        setMultiplier(1)
     };
 
     const defineHighscore = () => {
         if (highscore < score) {
             setHighscore(score)}
-    }
+    };
 
     const player = {
         name: name,
@@ -64,35 +65,50 @@ function PlayerStats() {
         lifes: lifes,
         multiplier: multiplier,
         highscore: highscore
-    }
-
-    function newPrompt() {
-    setPrompt(generatePrompt());
-    
-    var id = window.setTimeout(function() {}, 0);
-    while (id--) {
-        window.clearTimeout(id);
-    };
-    
-        const timer = setTimeout(function() {
-            resetStreak() 
-            newPrompt()
-        },  prompt.time);
     };
     
     useEffect(() => {
-    
+        
+        function newPrompt() {
+            if (lifesRef.current > 1) {
+                setPrompt(generatePrompt());
+                var id = window.setTimeout(function() {}, 0);
+                while (id--) {
+                    window.clearTimeout(id);
+                };
+            
+                const timer = setTimeout(function() {
+                    if (lifesRef.current > 0) {
+                        resetStreak()
+                        newPrompt()
+                        decrementLife(1)
+                    };
+                },  prompt.time);
+            }
+            else {
+                setPrompt({message: 'You Lost'})
+            }
+
+        };
+
         function checkKey(event) {
-        if (event.keyCode == prompt.code) {
-            incrementScore(prompt.value);
-            incrementStreak(1)
-            newPrompt()
-        }
-        else {
-            resetStreak()
-            newPrompt()
-            decrementLife(1)
-        }
+            if (prompt.code == undefined) {
+                newPrompt()
+            }
+            else if (event.keyCode == prompt.code && lifesRef.current > 0) {
+                incrementScore(prompt.value * multiplier);
+                incrementStreak(1)
+                newPrompt()
+            }
+            else if (event.keyCode != prompt.code && lifesRef.current > 0) {
+                resetStreak()
+                newPrompt()
+                decrementLife(1)
+            }
+            else if (event.keyCode == prompt.code && lifesRef.current == 0 || lifesRef.current < 0) {
+                newPrompt()
+            }
+
         };
     
         window.addEventListener("keydown", checkKey);
@@ -101,9 +117,7 @@ function PlayerStats() {
         window.removeEventListener("keydown", checkKey);
         };
     }, [prompt]);
-
-    
-    
+ 
     return(
         <>
             <div id='scoreboard' style={{display: 'block'}}>
@@ -116,14 +130,21 @@ function PlayerStats() {
                 <div id='lifes'>Lifes: {player.lifes}</div>
                 <div id='highscore'>Highscore: {player.highscore}</div> 
             </div>
-
             <div id='prompt'>
-            <p>{prompt.message}</p>
-            <button onClick={() => newPrompt()}>Start</button>
+                <p>{prompt.message}</p>
+                <button onClick={ 
+                    function() {
+                        resetLife()
+                        resetStreak()
+                        resetMultiplier()
+                        resetScore()
+                        setPrompt({time: 5000})
+                    }}>
+                    Start
+                    </button>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default PlayerStats
-// export { incrementScore, decrementScore, incrementStreak, resetStreak, incrementLife, decrementLife, incrementMultiplier, decrementMultiplier, resetMultiplier, setHighscore };
