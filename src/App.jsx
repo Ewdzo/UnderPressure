2684e6cffe5693322efafa3e7f3f628024fea952
Enzo Weder <enzowederma@gmail.com>
2022-08-02 18:54:09 -0300

    ✨ feat(add new difficulty selector): add new difficulty
    Incorporate MainPage into App.jsx
    Adds a visual feature that indicates the current difficulty selected by the player

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,55 @@
-import './App.css'
-import {useState} from 'react'
-import UnderPressure from './components/player'
-import MainScreen from './components/mainscreen';
+import './App.css';
+import UnderPressure from './components/player';
+import { useEffect, useState } from 'react';
+
 
 function App() {
-    // document.getElementById(start).onclick = {setPlaying(true)} 
-    // const [playing, setPlaying] = useState(false);
-    const playing = false;
+  const [playing, setPlaying] = useState(1);
 
-  return (
-    <>
-      {playing
-        ? <UnderPressure />
-        : <MainScreen /> }
-    </>
-  )
+  useEffect(() => {
+    const labels = document.getElementsByTagName('label')
+
+    function setUnderline(index) {
+      for (var i=0; i != 4; i++) {
+        document.getElementsByTagName('label')[i].style.textDecoration = 'none';
+        document.getElementsByTagName('label')[index].style.textDecoration = 'underline';
+      }
+    }
+
+    [...labels].forEach((label, index) => { 
+      labels[index].onclick = () => {
+        setUnderline(index)
+      }
+    });
+  })
+
+  if (playing == 1) { 
+    return (
+      <>
+        <div id="main">
+          <div id='menu'><a href=""><i className="fa fa-bars"></i></a></div>
+          <div id='profile'><a href=""><img src="src/images/default_icon.png" alt="" /></a></div>
+          <div id='title'><h1>Under Pressure</h1></div>
+          <div id='prompt'>
+              <button id="start" onClick={() => {setPlaying(2)}}>Play</button>
+              <form id="difficulty-selector" action="">
+                <label htmlFor="easy">Easy</label>
+                <label htmlFor="medium">Medium</label>
+                <label htmlFor="hard">Hard</label>
+                <label htmlFor="insane">Insane</label><br />
+                <input type="radio" id="easy" name="difficulty" value="Easy" />
+                <input type="radio" id="medium" name="difficulty" value="Medium" />
+                <input type="radio" id="hard" name="difficulty" value="Hard" />  
+                <input type="radio" id="insane" name="difficulty" value="Insane" />    
+              </form> 
+            </div> 
+        </div>
+      </>
+  )}
+
+  else if (playing == 2) {
+    return (<UnderPressure />)
+  }
 };
 
 export default App
