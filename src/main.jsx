9cc7beb8998963a6dca36accf6ca33ce38815534
Enzo Weder <enzowederma@gmail.com>
2022-08-04 18:17:53 -0300

    ✨ feat: add video background

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import VideoBackground from './components/VideoBackground';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <VideoBackground />
     <App />
   </React.StrictMode>
 )
