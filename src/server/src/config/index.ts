c4071e0a2d869640ce577f950fefb8841d8f171c
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2022-08-04 17:24:11 -0300

    ✨ feat: .env added
    .env were added on root

--- src/server/src/config/index.ts
+++ src/server/src/config/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
+import * as path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, "../../../../.env") });
 
 export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
