9b6bb848509ba29f0140de4c476616650563e3cc
Enzo Weder <enzowederma@gmail.com>
2022-08-05 00:34:31 -0300

    🌈 style: profile menu display icons

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import './profile.css'
 
 function Profile() {
-    const isLogged = true;
+    const isLogged = false;
 
     const [user, setUser] = useState('Ewdzo')
     const [userHighscore, setUserHighscore] = useState(100000)
@@ -37,13 +37,19 @@ function Profile() {
                 <div id="profile-container">
                     <a href={`https://github.com/${user}`} target='blank'><img id="profile-picture" src={`https://github.com/${user}.png`}  alt="" /></a>
                     <h1>{user}</h1>
-                    <ul>
-                        <li><h3>Highscore</h3><br />{userHighscore}</li>
-                        <li><h3>Biggest Streak</h3><br />{userStreak}</li>
-                        <li><h3>Biggest Multiplier</h3><br />{userMultiplier}</li>
-                        <li><h3>Matches Played</h3><br />{userMatchs}</li>
-                        <li><h3>Most Played Difficulty</h3><br />{userDifficulty}</li>
-                    </ul>
+                    <table>
+                        <tr>
+                            <td><img src="src/images/highscore.png" alt="Highscore Icon" /><h3>Highscore</h3><br />{userHighscore}</td>
+                            <td><img src="src/images/streak.png" alt="Streak Icon" /><h3>Biggest Streak</h3><br />{userStreak}</td>
+                        </tr>
+                        <tr>
+                            <td><img src="src/images/multiplier.png" alt="Multiplier Icon" /><h3>Biggest Multiplier</h3><br />{userMultiplier}</td>
+                            <td><img src="src/images/joystick.png" alt="Joystick Icon" /><h3>Matches Played</h3><br />{userMatchs}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><img src="src/images/difficulty.png" alt="Difficulty Icon" /><h3>Most Played Difficulty</h3><br />{userDifficulty}</td>
+                        </tr>
+                    </table>
                     <button id="log-out"><img src="src/images/log-out.png" alt="" title="Log Out"/></button>
                 </div>
             </>
@@ -52,7 +58,7 @@ function Profile() {
     else if(isLogged == false) {
         return ( 
             <>  
-                <div id='menu'><input id="menu-btn" type='checkbox' /><i className="fa fa-bars"></i></div>  
+                <div id='menu'><input id="menu-btn" type='checkbox' /><img id="menu-icon" src="src/images/hamburger_icon.png" alt="" /></div>    
                 <div id="profile-container">
                     <a href=""><img id="profile-picture" src='src/images/default_icon.png'  alt="" /></a>
                     <h1>Welcome, Guest!</h1>
