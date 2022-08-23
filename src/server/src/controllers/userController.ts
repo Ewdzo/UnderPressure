eb921f06b9ccc0b28dead9024b1ec319b1e401de
Enzo Weder <enzowederma@gmail.com>
2022-08-23 20:33:05 -0300

    🦄 refactor: refactor userController.ts

--- src/server/src/controllers/userController.ts
+++ src/server/src/controllers/userController.ts
@@ -9,10 +9,12 @@ export class UserController {
         
         try {
             createUser(userToken, tableName as string);
-        } catch (error) {
+        }
+        catch (error) {
             res.status(400).send({ error: error })
         }
     }
+
     public updateUser(req: Request, res: Response) {
         const userToken = req.body.data.userToken;
         const score = req.body.data.score;
@@ -23,17 +25,20 @@ export class UserController {
         
         try {
             updateUser(userToken, tableName as string, score, streak, multiplier as string, difficulty);
-        } catch (error) {
+        } 
+        catch (error) {
             res.status(400).send({ error: error })
         }
     }
-    public async getUserData(req: Request, res: Response) {
+
+    public getUserData(req: Request, res: Response) {
         const userToken = req.get('userToken');
         const tableName = process.env.TABLE_NAME;
         
         try {
             getUserData(userToken as string, tableName as string, (results: any) => {res.status(200).send(results)} )
-        } catch (error) {
+        } 
+        catch (error) {
             console.log(error)
             res.status(400).send({ error: error })
         }
