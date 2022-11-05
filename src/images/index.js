6fb266cf826313c76acbf04af09a0e630ffe0c8d
Enzo Weder <enzowederma@gmail.com>
2022-11-05 09:28:08 -0300

    ✨ feat: add images array to import

---
+++ src/images/index.js
@@ -0,0 +1,41 @@
+import Hearts0 from "./hearts_0.png";
+import Hearts1 from "./hearts_1.png";
+import Hearts2 from "./hearts_2.png";
+import Hearts3 from "./hearts_3.png";
+import BackgroundBlue from "./background_blue.png";
+import BorderBlue from "./border_blue.png";
+import BorderPurple from "./border_purple.png";
+import Countdown0 from "./countdown_0.png";
+import Countdown1 from "./countdown_1.png";
+import Countdown2 from "./countdown_2.png";
+import Countdown3 from "./countdown_3.png";
+import Countdown4 from "./countdown_4.png";
+import Countdown5 from "./countdown_5.png";
+import DefaultIcon from "./default_icon.png";
+import DifficultyIcon from "./difficulty.png"
+import GitHub from "./github.png";
+import Heart from "./heart.png";
+import Highscore from "./highscore.png";
+import HomeBlue from "./home_blue.png";
+import HomePurple from "./home_purple.png";
+import Joystick from "./joystick.png";
+import LogOutBlue from "./log-out_blue.png";
+import LogOutPurple from "./log-out_blue.png";
+import LogOutHoverBlue from "./log-out-hover_blue.png";
+import LogOutHoverPurple from "./log-out-hover_blue.png";
+import MenuBlue from "./menu_blue.png";
+import MenuPurple from "./menu_purple.png";
+import Multiplier from "./multiplier.png";
+import NewHeart from "./new_heart.png";
+import Palette from "./palette.png";
+import Refresh from "./refresh.png";
+import Streak from "./streak.png";
+import ThemeBlue from "./theme_1.png";
+import ThemePurple from "./theme_2.png";
+import UnderPressureBlue from "./under_pressure_blue.png";
+import UnderPressurePurple from "./under_pressure_purple.png";
+import UnderPressureRed from "./under_pressure_red.png";
+
+const images = [ Hearts0, Hearts1, Hearts2, Hearts3, BackgroundBlue, BorderBlue, BorderPurple, Countdown0, Countdown1, Countdown2, Countdown3, Countdown4, Countdown5, DefaultIcon, DifficultyIcon, GitHub, Heart, Highscore, HomeBlue, HomePurple, Joystick, LogOutBlue, LogOutPurple, LogOutHoverBlue, LogOutHoverPurple, MenuBlue, MenuPurple, Multiplier, NewHeart, Palette, Refresh, Streak, ThemeBlue, ThemePurple, UnderPressureBlue, UnderPressurePurple, UnderPressureRed];
+
+export default images;
