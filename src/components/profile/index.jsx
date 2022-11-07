df018b990145c842e514164dc669edcefc224345
Enzo Weder <enzowederma@gmail.com>
2022-11-07 10:09:02 -0300

    🎈 perf: add proper image import
    Add proper image import to profile/index.jsx

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -3,6 +3,7 @@ import { getCookie, userToken } from "../cookies";
 import { userGitInfo } from "./data";
 import Axios from "axios";
 import './profile.css'
+import images from "../../images";
 
 function Profile() {
     if(userToken) {
@@ -15,6 +16,7 @@ function Profile() {
         const [userMatches, setUserMatches] = useState(userData.matches);
         const [userDifficulty, setUserDifficulty] = useState(userData.difficulty);
         const [currentTheme, setTheme] = useState('blue');
+        const [themeIndex, setThemeIndex] = useState('0');
 
         const playerRegister = () => {
             if(userToken) {
@@ -52,9 +54,22 @@ function Profile() {
             };
         };
 
+        const updateTheme = () => {
+            if(currentTheme == 'blue'){ setThemeIndex(0) }
+            else if(currentTheme == 'purple'){ setThemeIndex(1) };
+    
+            if(document.getElementById('logo')) {document.getElementById('logo').src = images[34 + themeIndex]};
+            if(document.getElementById('home-icon')) {document.getElementById('home-icon').src = images[18 + themeIndex]};
+            if(document.getElementById('profile-logo')) {document.getElementById('profile-logo').src = images[34 + themeIndex]};
+            if(document.getElementById('log-out-img')) {document.getElementById('log-out-img').src = images[21 + themeIndex]}
+            if(document.getElementById('log-out-img-hover')) {document.getElementById('log-out-img-hover').src = images[23 + themeIndex]}
+            if(document.getElementById('menu-icon')) {document.getElementById('menu-icon').src = images[25 + themeIndex]}
+        };
+
         useEffect(() => {
             updateProfile();
             playerRegister(); 
+            updateTheme();
 
             if(userToken && !userData.synced) {
                 setUserData({synced: true});
@@ -117,28 +132,28 @@ function Profile() {
 
         return ( 
             <>
-                <div id='menu'><input id="menu-btn" type='checkbox' /><img id="menu-icon" src={`src/images/menu_${currentTheme}.png`} alt="Menu Icon" /></div>            
+                <div id='menu'><input id="menu-btn" type='checkbox' /><img id="menu-icon" src={images[25 + themeIndex]} alt="Menu Icon" /></div>            
                 <div id="profile-container">
                     <a href={`https://github.com/${userGitInfo.login}`} target='blank'><img id="profile-picture" src={avatar}  alt="Profile Picture" /></a>
                     <h1>{user}</h1>
                     <table>
                         <tbody>
                         <tr>
-                            <td><img src="src/images/highscore.png" alt="Highscore Icon" /><h3>Highest Score</h3><span>{userHighscore}</span></td>
-                            <td><img src="src/images/streak.png" alt="Streak Icon" /><h3>Highest Streak</h3><span>{userStreak}</span></td>
+                            <td><img src={images[17]} alt="Highscore Icon" /><h3>Highest Score</h3><span>{userHighscore}</span></td>
+                            <td><img src={images[31]}alt="Streak Icon" /><h3>Highest Streak</h3><span>{userStreak}</span></td>
                         </tr>
                         <tr>
-                            <td><img src="src/images/multiplier.png" alt="Multiplier Icon" /><h3>Highest Multiplier</h3><span>{userMultiplier}</span></td>
-                            <td><img src="src/images/joystick.png" alt="Joystick Icon" /><h3>Matches Played</h3><span>{userMatches}</span></td>
+                            <td><img src={images[27]} alt="Multiplier Icon" /><h3>Highest Multiplier</h3><span>{userMultiplier}</span></td>
+                            <td><img src={images[20]} alt="Joystick Icon" /><h3>Matches Played</h3><span>{userMatches}</span></td>
                         </tr>
                         <tr>
-                            <td colSpan={2}><img src="src/images/difficulty.png" alt="Difficulty Icon" /><h3>Last Played Difficulty</h3><span>{userDifficulty}</span></td>
+                            <td colSpan={2}><img src={images[14]} alt="Difficulty Icon" /><h3>Last Played Difficulty</h3><span>{userDifficulty}</span></td>
                         </tr>
                         </tbody>
                     </table>
                     <div id='buttons'>
-                        <button id="log-out"><img id="log-out-img" src={`src/images/log-out_${currentTheme}.png`} alt="Log Out Icon" title="Log Out"/><img id="log-out-img-hover" src={`src/images/log-out-hover_${currentTheme}.png`} alt="Log Out Icon" title="Log Out"/></button>
-                        <button id="refresh"><img src="src/images/refresh.png" alt="Refresh Icon" title="Refresh Profile"/></button>
+                        <button id="log-out"><img id="log-out-img" src={images[21 + themeIndex]} alt="Log Out Icon" title="Log Out"/><img id="log-out-img-hover" src={images[23 + themeIndex]} alt="Log Out Icon" title="Log Out"/></button>
+                        <button id="refresh"><img src={images[30]} alt="Refresh Icon" title="Refresh Profile"/></button>
                     </div>
                 </div>
             </>
@@ -146,6 +161,19 @@ function Profile() {
     }
     else {
         const [currentTheme, setTheme] = useState('blue');
+        const [themeIndex, setThemeIndex] = useState('0');
+
+        const updateTheme = () => {
+            if(currentTheme == 'blue'){ setThemeIndex(0) }
+            else if(currentTheme == 'purple'){ setThemeIndex(1) };
+    
+            if(document.getElementById('logo')) {document.getElementById('logo').src = images[34 + themeIndex]};
+            if(document.getElementById('home-icon')) {document.getElementById('home-icon').src = images[18 + themeIndex]};
+            if(document.getElementById('profile-logo')) {document.getElementById('profile-logo').src = images[34 + themeIndex]};
+            if(document.getElementById('log-out-img')) {document.getElementById('log-out-img').src = images[21 + themeIndex]}
+            if(document.getElementById('log-out-img-hover')) {document.getElementById('log-out-img-hover').src = images[23 + themeIndex]}
+            if(document.getElementById('menu-icon')) {document.getElementById('menu-icon').src = images[25 + themeIndex]}
+        };
 
         useEffect(() => {
             const menuCheckbox = document.getElementById('menu-btn');
@@ -165,18 +193,20 @@ function Profile() {
             };
 
             if(document.querySelector('input[name="theme"]:checked')) { setTheme(document.querySelector('input[name="theme"]:checked').value)}
+        
+            updateTheme();
         });
 
         return ( 
             <>  
-                <div id='menu'><input id="menu-btn" type='checkbox' /><img id="menu-icon" src={`src/images/menu_${currentTheme}.png`} alt="Menu Icon" /></div>    
+                <div id='menu'><input id="menu-btn" type='checkbox' /><img id="menu-icon" src={images[25 + themeIndex]} alt="Menu Icon" /></div>    
                 <div id="profile-container">
-                    <a href=""><img id="profile-picture" src='src/images/default_icon.png'  alt="Profile Icon" /></a>
+                    <a href=""><img id="profile-picture" src={images[13]}  alt="Profile Icon" /></a>
                     <h1>Welcome, Guest!</h1>
-                    <div id="game-logo"><img id="profile-logo" src={`src/images/under_pressure_${currentTheme}.png`} alt="Under Pressure Icon" /></div>
+                    <div id="game-logo"><img id="profile-logo" src={images[34 + themeIndex]} alt="Under Pressure Icon" /></div>
                     <a href="http://localhost:8000/auth">
                         <div id="log-in">
-                            <img id="git-logo" src="src/images/github.png" alt="GitHub Icon" />
+                            <img id="git-logo" src={images[15]} alt="GitHub Icon" />
                             <p>Log in with GitHub</p>
                         </div>
                     </a>
